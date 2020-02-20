ba2768b5681f9562fab0a5d33a914198ff5aa8f7
Jason Schloer <jschloer@hotmail.com>
2020-02-20 10:56:00 -0500

    Updates

---
+++ precache-manifest.b437b990d612523731c3e5882b1db250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2339ac8308320eac0c58efebd55c8b",
+    "url": "/use-multiselect/index.html"
+  },
+  {
+    "revision": "e37ea93ecb7fcfbeda32",
+    "url": "/use-multiselect/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "c90b939c60fcdfc64dd1",
+    "url": "/use-multiselect/static/js/2.2e946457.chunk.js"
+  },
+  {
+    "revision": "e37ea93ecb7fcfbeda32",
+    "url": "/use-multiselect/static/js/main.cd149221.chunk.js"
+  },
+  {
+    "revision": "dd32406e316d7ec36916",
+    "url": "/use-multiselect/static/js/runtime-main.e6f16807.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/use-multiselect/precache-manifest.976c97693f435c33c360f8c1e4fcc3a2.js"
+  "/use-multiselect/precache-manifest.b437b990d612523731c3e5882b1db250.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/use-multiselect/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/use-multiselect/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
